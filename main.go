3e681b0d4478735183c84783b6820d33274efc7f
sayandas-sd <sayandasmac@gmail.com>
2024-12-29 00:40:30 +0530

    added http endpoint for creating blocks

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import(
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func main() {
+	r := mux.NewRouter()
 
-func main(){
-	
+	r.HandleFunc("/", getBLock).Methods("GET")
+	r.HandleFunc("/", writeBlock).Methods("POST")
+	r.HandleFunc("/", newBlock).Methods("POST")
+
+	log.Println("Server is running on port: 8080")
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
 }
